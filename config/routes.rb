28d0733a07dc28cfd1e665dba8881d6030a52a95
Will Groehler <1497992+wgrhl@users.noreply.github.com>
2020-10-22 13:09:48 -0700

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome_page/welcome'
+  root 'welcome_page#welcome'
 end
